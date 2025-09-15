9feaad017097983ae401a0cb1b1217b4b756c76c
dinesh-bazgain <dinesh.bazgain@gmail.com>
2025-09-15 11:02:55 +0545

    refactor: added the theme

--- gunaso_frontend/src/app/layout.js
+++ gunaso_frontend/src/app/layout.js
@@ -11,7 +11,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="cds-theme-zoning">
-        <Headers />
         <Providers>{children}</Providers>
       </body>
     </html>
